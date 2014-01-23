e6b279219dd1d744b80a6323db03c2fae7d71591
Antti Rasinen <antti.rasinen@codento.com>
2014-01-23 21:24:37 +0200

    At least it compiles

--- elmloader.js
+++ elmloader.js
@@ -1,7 +1,10 @@
 var vm = require("vm");
 var fs = require("fs");
 
-var sandbox = { document: require('./document-wrapper.js') };
+var sandbox = { document: require('./document-wrapper.js'),
+                window: {},
+ 		setTimeout: function(f, n) {f()},
+	};
 
 module.exports = function(runtimepath, modulepath) {
   var runtime = fs.readFileSync(runtimepath);
