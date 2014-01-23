cda76153a5e51c86609bd8d9d21e6e15aa970936
Antti Rasinen <antti.rasinen@codento.com>
2014-01-23 21:34:21 +0200

    Fix printing of received values

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var w = Elm.worker(Elm.Squarer, {inputs: 0})
 
 console.log("Attempting to send")
 
-w.ports.outputs.subscribe(function(e) {console.log("Got", e.value)});
+logger = function(e) {console.log("Got", e)};
+w.ports.outputs.subscribe(logger);
 
 for (var k = 0; k < 10; k++)
 	w.ports.inputs.send(k);
